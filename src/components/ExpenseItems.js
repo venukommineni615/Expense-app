5e5fd8490e0097019832544d6eed026f073bff86
venu kommineni <venukommineni615@gmail.com>
2024-01-09 23:24:42 +0530

    created a new ExpenseItems component

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems(){
+    return(
+        <div>
+            <h2>Expense Items</h2>
+            <ul>
+                <li>Food Rs 10</li>
+                <li>Petrol Rs 100</li>
+                <li>Movies Rs 200</li>
+            </ul>
+        </div>
+    )
+}
+export default ExpenseItems
