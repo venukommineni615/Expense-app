68c3f63402a957f4117d0c08e3d68d0fc7f2d43a
venu kommineni <venukommineni615@gmail.com>
2024-01-11 12:22:49 +0530

    added a new button to change the price

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,7 +1,12 @@
+import React,{useState} from "react"
 import "./ExpenseItems.css"
 import ExpenseDate from "./ExpenseDate"
 import ExpenseDetails from "./ExpenseDetails"
 const ExpenseItems=(props)=>{
+    const [price,setPrice]=useState(props.amount)
+    const newPrice=()=>{
+        setPrice(100)
+    }
     const deleteElement=(event)=>{
         event.target.parentElement.remove()
     }
@@ -10,8 +15,9 @@ const ExpenseItems=(props)=>{
 
         <ExpenseDate date ={props.date} />
         
-        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} expense={props.expense} />
+        <ExpenseDetails amount={price}  location={props.location}   title={props.title} expense={props.expense} />
         <button onClick={deleteElement}>Delete</button>
+        <button onClick={newPrice}>Change Expense</button>
         
         </div>
     )
