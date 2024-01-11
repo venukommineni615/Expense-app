9bec7aa414502bb494cbb0561f561150ec24ffa8
venu kommineni <venukommineni615@gmail.com>
2024-01-11 20:02:14 +0530

    on submit is handled for form submission

--- src/components/Expenses/AddExpenses.js
+++ src/components/Expenses/AddExpenses.js
@@ -7,26 +7,34 @@ const AddExpense=()=>{
     const [title,setTitle]=useState("")
     const [expense,setExpense]=useState("")
     const dataSubmitted=(event)=>{
-        setTitle(event.target.title.value)
-        setDate(event.target.date.value)
-        setExpense(event.target.expense.value)
-        setLocation(event.target.location.value)
-        setPrice(event.target.price.value)
+        event.preventDefault()
+        
+        // setTitle(event.target.title.value)
+        // setDate(event.target.date.value)
+        // setExpense(event.target.expense.value)
+        // setLocation(event.target.location.value)
+        // setPrice(event.target.price.value)
+        console.log(event.target.title.value)
+        console.log(event.target.mydate.value)
+        console.log(event.target.expense.value)
+        console.log(event.target.location.value)
+        console.log(event.target.price.value)
+
     }
     const printValue=(event)=>{
         console.log(event.target.value)
     }
     return(
-        <form>
+        <form onSubmit={dataSubmitted}>
             <label htmlFor="date">Date:</label>
-            <input type="date" id="date" name="date" onChange={printValue}required></input>
+            <input type="date" id="date" name="mydate" onChange={printValue}required></input>
             <label htmlFor="expense">Expense:</label>
             <input type="text" id="expense" name="expense" onChange={printValue} required></input>
             <label htmlFor="location">Location:</label>
             <input type="text" id="location" name='location' onChange={printValue} required></input>
             <label htmlFor="price">Price:</label>
-            <input type="number" id="price" name="price" onChange={printValue} required></input>
-            <button type="submit" onSubmit={dataSubmitted}>Submit</button>
+            <input type="number" id="price" name="price" onChange={printValue} required min="1"></input>
+            <button type="submit" >Submit</button>
 
         </form>
     )
