1bfe1ce9d5560354c58ef3353649d6d490e108c9
venu kommineni <venukommineni615@gmail.com>
2024-01-11 23:55:29 +0530

    key prop is added to components return by array.map method

--- src/App.js
+++ src/App.js
@@ -8,30 +8,35 @@ const App=()=>{
  
   const expenses = [
     {
+      id:Math.random(),
       date:new Date('2023-01-01'),
       expense: 'Groceries',
       expenseLocation: 'Supermarket',
       amount: 50
     },
     {
+      id:Math.random(),
       date:new Date('2023-02-15'),
       expense: 'Dinner',
       expenseLocation: 'Restaurant',
       amount: 30
     },
     {
+      id:Math.random(),
       date: new Date('2023-03-10'),
       expense: 'Transportation',
       expenseLocation: 'Gas Station',
       amount: 40
     },
     {
+      id:Math.random(),
       date: new Date('2023-04-22'),
       expense: 'Entertainment',
       expenseLocation: 'Movie Theatre',
       amount: 25
     },
     {
+      id:Math.random(),
       date: new Date('2023-05-05'),
       expense: 'Healthcare',
       expenseLocation: 'Pharmacy',
@@ -42,7 +47,7 @@ const App=()=>{
    const appendExpense=(newExpense)=>{
       expenses.push(newExpense)
       setMyexpenses((prevExpenses)=>{
-        return [...prevExpenses,newExpense]
+        return [newExpense, ...prevExpenses]
       })
    }
    const [filterYear,setFilterYear]=useState('2020')
@@ -56,7 +61,7 @@ const App=()=>{
       <ExpenseFilter selected={filterYear} changeFilter={dropdownYear}></ExpenseFilter>
       {myexpenses.map((ele)=>{
 
-        return (<ExpenseItems date={ele.date} expense={ele.expense} location={ele.expenseLocation} amount={ele.amount}></ExpenseItems>)
+        return (<ExpenseItems key={ele.id} date={ele.date} expense={ele.expense} location={ele.expenseLocation} amount={ele.amount}></ExpenseItems>)
       })}
     </div>
   );
